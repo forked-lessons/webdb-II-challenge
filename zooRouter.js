2017af98e015f2187626d7e2b7871710288fecad
Daniel <danielstarling4@gmail.com>
2019-03-26 13:52:43 -0700

    zoo put working

--- zooRouter.js
+++ zooRouter.js
@@ -51,6 +51,22 @@ router.post('/', (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+  zoodb('zoos')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: 'Record not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 router.delete('/:id', (req, res) => {
   // const zooId = req.params.id;
   zoodb('zoos')
