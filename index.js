38f87fd16981fe070e74e44363dd33cd8f590bc4
Daniel <danielstarling4@gmail.com>
2019-03-26 12:53:58 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const db = require('./knexfile');
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
-
+server.get('/', (req, res) => {
+  db('lambda');
+});
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
