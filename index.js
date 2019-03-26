868b8ca7df497efc6e368e5f7a70b45b8ed77974
Daniel <danielstarling4@gmail.com>
2019-03-26 13:01:18 -0700

    api connected

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
-const db = require('./knexfile');
 const server = express();
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
 server.get('/', (req, res) => {
-  db('lambda');
+  res.send('Hello');
 });
+
 const port = 3300;
+// server.use('/api/zoos', zooRouter);
+
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
